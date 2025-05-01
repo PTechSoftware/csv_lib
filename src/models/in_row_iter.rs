7e05f0106d394314245786af4f145240ca12915b
Nacho <nachopp98@gmail.com>
2025-04-30 22:37:41 -0300

    fixed cursor in row iter

--- src/models/in_row_iter.rs
+++ src/models/in_row_iter.rs
@@ -74,12 +74,15 @@ impl<'mmap> InRowIter<'mmap> {
     /// Extract the content of a field in raw format.
     pub fn get_field_index(&mut self, target: usize) -> Option<&'mmap [u8]> {
         let mut count = 0;
+        let actual = self.get_cursor();
         while let Some(el) = self.next(){
             if count == target {
+                self.set_cursor(actual);
                 return Some(el);
             }
             count = count + 1;
         }
+        self.set_cursor(actual);
         None
     }
 }
